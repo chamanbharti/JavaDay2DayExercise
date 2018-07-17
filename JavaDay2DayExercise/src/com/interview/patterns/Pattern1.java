fcdd1b751a2eb9f3c06c381ad78b075daf431674
chaman bharti <chaman.bharti84@gmail.com>
2018-07-17 12:40:32 +0530

    comment is added.

--- JavaDay2DayExercise/src/com/interview/patterns/Pattern1.java
+++ JavaDay2DayExercise/src/com/interview/patterns/Pattern1.java
@@ -6,9 +6,16 @@ public class Pattern1 {
 		
 		for(int i=1;i<=5;i++) {
 			for(int j=1;j<=5;j++) {
-				System.out.print(j);
+				System.out.print("*");
 			}
 			System.out.println();
 		}
 	}
 }
+/*
+*****
+*****
+*****
+*****
+*****
+ */
